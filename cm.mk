d1db9a69be198545bb6f0bef8d23b4dafb5f7129
Anik khan <anik9280@gmail.com>
2015-12-22 20:25:39 +0600

    sprout: update fingerprint from 6.0.1 release
    Change-Id: Iae97dcccddf5de403c721a02ddf2329c6708db83

--- cm.mk
+++ cm.mk
@@ -3,10 +3,10 @@ $(call inherit-product, device/google/sprout8/sprout8.mk)
 # Common CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0.1/MMB29K/2419427:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0.1 MMB29K 2419427 release-keys"
 
 PRODUCT_NAME := cm_sprout8
-PRODUCT_DEVICE :=sprout8
+PRODUCT_DEVICE := sprout8
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 PRODUCT_MODEL := A1
